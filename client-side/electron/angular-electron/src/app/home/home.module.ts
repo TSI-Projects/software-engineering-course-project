4a8381690505e5e43119aff853784318225c88f0
Andrejs Ponatovskis <andrejs.pon00@gmail.com>
2023-11-17 13:08:28 +0200

    Add header

--- client-side/electron/angular-electron/src/app/home/home.module.ts
+++ client-side/electron/angular-electron/src/app/home/home.module.ts
@@ -6,8 +6,15 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 
+
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+  declarations: [
+    HomeComponent,
+  ],
+  imports: [
+    CommonModule, 
+    SharedModule, 
+    HomeRoutingModule
+  ]
 })
 export class HomeModule {}
