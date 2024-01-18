bae3d21dd816c288c308fbc67f973c89636ef19f
Andrejs Ponatovskis <andrejs.pon00@gmail.com>
2024-01-18 13:01:27 +0200

    Cleanup

--- client-side/electron/angular-electron/src/app/room/room.component.ts
+++ client-side/electron/angular-electron/src/app/room/room.component.ts
@@ -18,29 +18,21 @@ export class RoomComponent implements OnInit {
   });
 
 
-  public images = [
-    { source: '../../assets/img/start-img.png', thumbnailImageSrc: '../../assets/img/thumbnail1.png' },
-    { source: '../../assets/img/our_rooms_1.png', thumbnailImageSrc: '../../assets/img/thumbnail2.png' },
-    { source: '../../assets/img/our_rooms_1.png', thumbnailImageSrc: '../../assets/img/thumbnail2.png' },
-    { source: '../../assets/img/our_rooms_1.png', thumbnailImageSrc: '../../assets/img/thumbnail2.png' },
-    { source: '../../assets/img/our_rooms_1.png', thumbnailImageSrc: '../../assets/img/thumbnail2.png' },
-    { source: '../../assets/img/our_rooms_2.png', thumbnailImageSrc: '../../assets/img/thumbnail3.png' }
-  ];
+  // public images = [
+  //   { source: '../../assets/img/start-img.png', thumbnailImageSrc: '../../assets/img/thumbnail1.png' },
+  //   { source: '../../assets/img/our_rooms_1.png', thumbnailImageSrc: '../../assets/img/thumbnail2.png' },
+  //   { source: '../../assets/img/our_rooms_1.png', thumbnailImageSrc: '../../assets/img/thumbnail2.png' },
+  //   { source: '../../assets/img/our_rooms_1.png', thumbnailImageSrc: '../../assets/img/thumbnail2.png' },
+  //   { source: '../../assets/img/our_rooms_1.png', thumbnailImageSrc: '../../assets/img/thumbnail2.png' },
+  //   { source: '../../assets/img/our_rooms_2.png', thumbnailImageSrc: '../../assets/img/thumbnail3.png' }
+  // ];
 
   constructor(
     private _roomsService: RoomsService
   ) { 
-    console.log(this.todayDate)
-    const today = new Date();
-    const tomorrow = new Date(today.getTime() + (24 * 60 * 60 * 1000));
-    this.dateRange.setValue({start: today, end: tomorrow});
+    const tomorrow = new Date(this.todayDate.getTime() + (24 * 60 * 60 * 1000));
+    this.dateRange.setValue({start: this.todayDate, end: tomorrow});
   }
-
-  myFilter = (d: Date | null): boolean => {
-    const today = new Date();
-    return d! >= today;
-  };
-
 
   async ngOnInit(): Promise<void> {
     const resp = await this._roomsService.loadRoom()
